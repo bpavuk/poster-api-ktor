c3491160ac88c68d5a110617f8bf8a9b109b9af6
bpavuk <boopentiy@gmail.com>
2023-04-17 21:13:43 +0300

    Added endpoint for swagger docs

--- src/main/kotlin/com/bpavuk/plugins/HTTP.kt
+++ src/main/kotlin/com/bpavuk/plugins/HTTP.kt
@@ -3,8 +3,13 @@ package com.bpavuk.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "/swagger", swaggerFile = "openapi/documentation.yaml")
+    }
     install(CORS) {
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
