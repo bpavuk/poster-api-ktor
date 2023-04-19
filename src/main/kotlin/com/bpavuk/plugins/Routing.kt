1ca15abd23fcafad8c29cd84e69ca982983be4d7
bpavuk <boopentiy@gmail.com>
2023-04-19 14:15:07 +0300

    Removed hello-world endpoint "/"

--- src/main/kotlin/com/bpavuk/plugins/Routing.kt
+++ src/main/kotlin/com/bpavuk/plugins/Routing.kt
@@ -4,14 +4,10 @@ import com.bpavuk.routing.helloRouting
 import com.bpavuk.routing.loginRouting
 import com.bpavuk.routing.userRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
         loginRouting()
         helloRouting()
         userRouting()
