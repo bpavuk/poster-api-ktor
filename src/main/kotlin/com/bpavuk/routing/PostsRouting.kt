bd2229b843481a1f02a6e64313fb42e7271ce32f
bpavuk <boopentiy@gmail.com>
2023-04-25 18:17:46 +0300

    Implemented route for getting uploaded files

--- src/main/kotlin/com/bpavuk/routing/PostsRouting.kt
+++ src/main/kotlin/com/bpavuk/routing/PostsRouting.kt
@@ -1,7 +1,12 @@
 package com.bpavuk.routing
 
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
+import java.io.File
 
 fun Route.postsRouting() {
     get("/posts/{post_id}") {
@@ -11,7 +16,20 @@ fun Route.postsRouting() {
 
     }
     get("/uploads/{user_id}/{file_name}") {
+        val userId = call.parameters.getOrFail("user_id").toInt()
+        val fileName = call.parameters.getOrFail("file_name")
 
+        val file = File("uploads/$userId/$fileName")
+        if (!file.exists()) {
+            call.respond(HttpStatusCode.NotFound, "File not found :p")
+        } else {
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, fileName)
+                    .toString()
+            )
+            call.respondFile(file)
+        }
     }
     authenticate {
         post("/posts/new") {
