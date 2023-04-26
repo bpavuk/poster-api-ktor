cc31b6366c526797e20c05c3db46e1077f665379
bpavuk <boopentiy@gmail.com>
2023-04-26 15:22:35 +0300

    Implemented /posts/new endpoint (fucking big piece of code, right?)

--- src/main/kotlin/com/bpavuk/routing/PostsRouting.kt
+++ src/main/kotlin/com/bpavuk/routing/PostsRouting.kt
@@ -2,12 +2,16 @@ package com.bpavuk.routing
 
 import com.bpavuk.dao.dao
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
 import java.io.File
+import java.util.*
 
 fun Route.postsRouting() {
     get("/posts/{post_id}") {
@@ -39,7 +43,41 @@ fun Route.postsRouting() {
     }
     authenticate {
         post("/posts/new") {
+            val principal = call.principal<JWTPrincipal>()
+            val id = principal!!.payload.getClaim("id").asInt()
+            val postMultipart = call.receiveMultipart()
+            val photosList = mutableListOf<String>()
+            var description = ""
 
+            postMultipart.forEachPart { part ->
+                when (part) {
+                    is PartData.FileItem -> {
+                        val match = "(.*)\\.(jpg|jpeg|png|gif)".toRegex().matchEntire(part.originalFileName!!) as MatchResult
+                        if (match.value == part.originalFileName) {
+                            val fileName = "${UUID.randomUUID()}.${match.groupValues[1]}"
+                            val fileBytes = part.streamProvider().readBytes()
+                            val file = File("./uploads/$id/$fileName")
+                            if (!file.parentFile.exists()) file.parentFile.mkdirs()
+                            file.createNewFile()
+                            file.writeBytes(fileBytes)
+                            photosList.add(fileName)
+                        } else {
+                            throw IllegalArgumentException(
+                                s = "Wrong file extensions! Only .jpg, .jpeg, .png and .gif are allowed"
+                            )
+                        }
+                    }
+                    is PartData.FormItem -> {
+                        description = part.value
+                    }
+                    else -> {
+                        throw IllegalArgumentException(s = "Unexpected element")
+                    }
+                }
+                part.dispose()
+            }
+
+            call.respond(dao.newPost(photos = photosList, postDescription = description, userId = id))
         }
         delete("/posts/{post_id}") {
 
